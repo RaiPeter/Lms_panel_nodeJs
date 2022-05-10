e83df95044fa3aea3138af1e2c5f3985b40cdadd
Peter <rai.peter2@gmail.com>
2022-05-10 11:38:40 +0530

    added env.PORt

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const hbs = require('hbs');
 const cors = require('cors')
 const cookie_Parser = require('cookie-parser');
