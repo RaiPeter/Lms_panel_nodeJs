60843ac2fbeb395d17ebdf83f72bf9452ac5c6ad
Peter <rai.peter2@gmail.com>
2022-05-10 11:29:16 +0530

    make it beter

--- app.js
+++ app.js
@@ -2,32 +2,40 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const path = require('path');
-const port =  3000;
-const hbs =  require('hbs');
+const port = 3000;
+const hbs = require('hbs');
+const cors = require('cors')
 const cookie_Parser = require('cookie-parser');
 app.use(cookie_Parser())
+const corsOptions = {
+    origin: "https://www.kalakarist.com"
+};
 // db connection 
 require('./server/db/conn')
 
 // paths 
-const static_Path =  path.join(__dirname, 'public');
-const template_Path =  path.join(__dirname, 'template/views');
-const partials_Path =  path.join(__dirname, 'template/partials');
+const static_Path = path.join(__dirname, 'public');
+const template_Path = path.join(__dirname, 'template/views');
+const partials_Path = path.join(__dirname, 'template/partials');
 
 // midleware 
 app.use(express.static(static_Path))
-
-
+app.use(cors(corsOptions))
 // template engine 
 app.set('view engine', 'hbs');
 app.set('views', template_Path);
 hbs.registerPartials(partials_Path);
 
 
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "www.kalakarist.com");
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 // routes 
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 const routes = require('./server/routes/webRoutes');
 const cookieParser = require('cookie-parser');
 app.use('/', routes);
@@ -37,6 +45,6 @@ const api_routes = require('./server/routes/apiRoutes');
 app.use('/api', api_routes);
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`listing at http://localhost:${port}`);
 })
--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -19,7 +19,8 @@ apirouter.get('/courses/:flag', async (req, res) => {
     try {
         console.log(req.params);
         const flag = req.params.flag;
-        const ivdCourse_data = await Course_model.findOne({ flag: flag, status: 'Active' }).select({ _id: 0, status: 0, date: 0, __v: 0 });
+        const ivdCourse_data = await Course_model.findOne({ flag: flag, status: 'Active' })
+            .select({ status: 0, date: 0, __v: 0 });
         console.log('invds ', ivdCourse_data);
         res.json(ivdCourse_data)
     } catch (e) {
@@ -29,7 +30,7 @@ apirouter.get('/courses/:flag', async (req, res) => {
 })
 
 // ========= all class by get ==========
-apirouter.get('/class/:course', async (req, res) => {
+apirouter.get('/class/:course', api_auth, async (req, res) => {
     try {
         console.log(req.params);
         const course = req.params.course;
@@ -42,7 +43,7 @@ apirouter.get('/class/:course', async (req, res) => {
     }
 })
 // =========== singel class by get===========
-apirouter.get('/class_invd/:id', async (req, res) => {
+apirouter.get('/class_invd/:id', api_auth, async (req, res) => {
     try {
         console.log(req.params);
         const id = req.params.id;
@@ -54,30 +55,7 @@ apirouter.get('/class_invd/:id', async (req, res) => {
         res.status(400).send(e);
     }
 })
- 
 
-// ===============
-// all class by post 
-apirouter.post('/class/:course',api_auth, async (req, res) => {
-    try {
-        const course = req.params.course;
-        const Class_data = await Class_model.find({ courseId: course }).select({ date: 0, __v: 0 });
-        res.json(Class_data)
-    } catch (e) {
-        res.send(e);
-    }
-})
-
-// single class by post 
-apirouter.post('/class_invd/:id',api_auth, async (req, res) => {
-    try {
-        const id = req.params.id;
-        const invdClass_data = await Class_model.find({ _id: id }).select({ date: 0, __v: 0 });
-        res.json(invdClass_data)
-    } catch (e) {
-        res.status(400).send(e);
-    }
-})
 
 
 
